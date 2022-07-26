3e00106112bd2c09702d6f4779502a0f86564017
zeminzhou <zhouzemin@pingcap.com>
2022-07-26 10:14:28 +0800

    update zshrc
    Signed-off-by: zeminzhou <zhouzemin@pingcap.com>

--- script/fzf-git.sh
+++ script/fzf-git.sh
@@ -5,7 +5,7 @@ is_in_git_repo() {
 }
 
 fzf-down() {
-  fzf --height 50% --min-height 20 --border --bind ctrl-/:toggle-preview "$@"
+  fzf --height 50% --min-height 20 --reverse --border --bind ctrl-/:toggle-preview "$@"
 }
 
 _gf() {
@@ -35,7 +35,7 @@ _gt() {
 _gh() {
   is_in_git_repo || return
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+  fzf-down --ansi --no-sort --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
   grep -o "[a-f0-9]\{7,\}"
